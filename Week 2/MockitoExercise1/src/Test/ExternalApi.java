c69bec38b3e00481d0eac995106494b0a79abe51
Sriram Arasu T <50352803+Sriramt384@users.noreply.github.com>
2025-06-29 21:04:44 +0530

    Mockito Excerise 1 Done

---
+++ Week 2/MockitoExercise1/src/Test/ExternalApi.java
@@ -0,0 +1,5 @@
+package Test;
+
+public interface ExternalApi {
+    String getData();
+}
---
+++ Week 2/MockitoExercise1/src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MockitoTest {
+	requires junit;
+	requires org.junit.jupiter.api;
+	requires org.mockito;
+}
